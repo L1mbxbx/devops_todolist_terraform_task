92a1442a4e0cb903ead3123f5bc4ebba7a478f40
L1mbxbx <ilya.msd2003@gmail.com>
2025-08-05 15:57:55 +0300

    Add compute module

---
+++ modules/compute/variables.tf
@@ -0,0 +1,46 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which the virtual machine will be created."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be created."
+  type        = string
+
+}
+
+variable "vm_name" {
+  description = "The name of the virtual machine."
+  type        = string
+  default     = "matebox"
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machine."
+  type        = string
+  default     = "Standard_B1s"
+
+}
+
+variable "admin_username" {
+  description = "The administrator username for the virtual machine."
+  type        = string
+  default     = "azureuser"
+
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key for the virtual machine."
+  type        = string
+  default     = "linuxboxsshkey"
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet where the virtual machine will be deployed."
+  type        = string
+}
+
+variable "public_ip_adress_id" {
+  description = "The ID of the public IP address associated with the virtual machine."
+  type        = string
+}
